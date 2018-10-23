9307527b314db2a316bd7c5b9257995dde9829e5
rummolprod999 <rummolprod999@gmail.com>
2018-10-23 12:28:28 +0300

    init

---
+++ script.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import traceback
+
+USER_NAME = '********'
+USER_PASS = '********'
+
+
+def main_fun():
+    options = Options()
+    options.add_argument("headless")
+    browser = Chrome(options=options)
+    try:
+        hh_worker(browser)
+    except Exception as ex:
+        print(ex)
+        traceback.print_exc()
+    finally:
+        browser.close()
+        browser.quit()
+
+
+def hh_worker(browser):
+    browser.delete_all_cookies()
+    browser.get("http://hh.ru/")
+    wait = WebDriverWait(browser, 20)
+    wait.until(EC.visibility_of_element_located(
+            (By.XPATH, "//label[@class = 'login-input']/input[contains(@name, 'username')]")),
+               'can not find input form on page')
+    browser.find_element(By.XPATH, "//label[@class = 'login-input']/input[contains(@name, 'username')]").send_keys(
+        USER_NAME)
+    browser.find_element(By.XPATH, "//label[@class = 'login-input']/input[contains(@name, 'password')]").send_keys(
+        USER_PASS)
+    browser.find_element(By.XPATH, "//div[@class = 'login-submit-form']/input[contains(@value, 'Войти')]").click()
+    resume_updater(browser, wait)
+
+
+def resume_updater(browser, wait):
+    browser.get("https://hh.ru/applicant/resumes")
+    wait.until(EC.visibility_of_element_located((By.XPATH, "//div[contains(@class, 'resumelist__resume')]")))
+    resume_list = browser.find_elements(By.XPATH, "//button[contains(@class, 'bloko-icon-link')]")
+    for res in resume_list:
+        try:
+            res.click()
+        except Exception as ex:
+            print(ex)
+    print("ok")
+
+
+if __name__ == '__main__':
+    main_fun()
